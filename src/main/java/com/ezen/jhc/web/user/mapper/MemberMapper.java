67e5e76baa5b729310a21959dade4194d12e2148
Gi7C-09 <dooha713@gmail.com>
2023-02-20 10:35:34 +0900

    mapper 예시

--- src/main/java/com/ezen/jhc/web/user/mapper/MemberMapper.java
+++ src/main/java/com/ezen/jhc/web/user/mapper/MemberMapper.java
@@ -1,7 +1,15 @@
 package com.ezen.jhc.web.user.mapper;
 
+import java.util.List;
+
+import com.ezen.jhc.web.user.dto.member.MemberDTO;
+
 public interface MemberMapper {
 	
+	public List<MemberDTO> getAll();
 	
+	public MemberDTO get(Integer mem_num);
+	
+	public Integer join(MemberDTO memberDTO);
 
 }
