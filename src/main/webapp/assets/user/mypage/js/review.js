c507370cb03e4a9c33a8dffbbba59cf58b1348c9
SUJEONG <SUJEONG@121.132.80.147>
2023-03-07 15:18:34 +0900

    test

---
+++ src/main/webapp/assets/user/mypage/js/review.js
@@ -0,0 +1,75 @@
+window.onload = function(){
+	function Rating(){};
+	Rating.prototype.rate = 0;
+	Rating.prototype.setRate = function(newrate){
+	   
+	    this.rate = newrate;
+	    let items = document.querySelectorAll('.rate_radio');
+	    items.forEach(function(item, idx){
+	        if(idx < newrate){
+	            item.checked = true;
+	        }else{
+	            item.checked = false;
+	        }
+	    });
+	}
+	let rating = new Rating();
+	
+	document.addEventListener('DOMContentLoaded', function(){
+    //별점선택 이벤트 리스너
+    document.querySelector('.check_box').addEventListener('click',function(e){
+        let elem = e.target;
+        if(elem.classList.contains('rate_radio')){
+            rating.setRate(parseInt(elem.value));
+        }
+    })
+});
+
+	
+    //상품평 작성 글자수 초과 체크 이벤트 리스너
+    document.querySelector('.review_textarea').addEventListener('keydown',function(){
+        //리뷰 400자 초과 안되게 자동 자름
+        let review = document.querySelector('.review_textarea');
+        let lengthCheckEx = /^.{400,}$/;
+        if(lengthCheckEx.test(review.value)){
+            //400자 초과 컷
+            review.value = review.value.substr(0,400);
+        }
+    });
+
+    //저장 전송전 필드 체크 이벤트 리스너
+    document.querySelector('.registration').addEventListener('click', function(e){
+        //별점 선택 안했으면 메시지 표시
+        if(rating.rate == 0){
+            rating.showMessage('rate');
+            return false;
+        }
+        //리뷰 5자 미만이면 메시지 표시
+        if(document.querySelector('.review_textarea').value.length < 5){
+            rating.showMessage('review');
+            return false;
+        }
+        //폼 서밋
+    });
+	
+	Rating.prototype.showMessage = function(type){//경고메시지 표시
+    switch(type){
+        case 'rate':
+            //안내메시지 표시
+            document.querySelector('.check_box .warning_msg').style.display = 'block';
+            //지정된 시간 후 안내 메시지 감춤
+            setTimeout(function(){
+                document.querySelector('.check_box .warning_msg').style.display = 'none';
+            },3000);            
+            break;
+        case 'review':
+            //안내메시지 표시
+            document.querySelector('.write_box .warning_msg').style.display = 'block';
+            //지정된 시간 후 안내 메시지 감춤
+            setTimeout(function(){
+                document.querySelector('.write_box .warning_msg').style.display = 'none';
+            },3000);    
+            break;
+    }
+}
+}
